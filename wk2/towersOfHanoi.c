a0dd8269af8bc2a98c32f1040b26dddbf53a7ee0
Josh <joshconstantinelim@gmail.com>
2024-06-03 19:26:13 +1000

    new tut question: towers of hanoi

---
+++ wk2/towersOfHanoi.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void solveHanoi(int numDisks, char *fromRod, char *toRod, char *otherRod) {
+    // TODO
+}
+
+int main(void) {
+    printf("Solving the Towers of Hanoi problem with 5 disks...\n");
+    solveHanoi(5, "A", "C", "B");
+    return 0;
+}
